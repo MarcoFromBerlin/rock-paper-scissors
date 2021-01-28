92acc66dd7ea7cc5e46f071259606b787c8e7579
Marco Vignotto <marcovignotto81@gmail.com>
2021-01-28 13:30:28 +0100

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,14 @@
 // - paper(2) beats rock(1)
 // - scissors(3) beats paper(2)
 
+// DOM
+const playerName = document.querySelector(".player");
+const btnRock = document.querySelector(".btn-rock");
+const btnPaper = document.querySelector(".btn-paper");
+const btnScissors = document.querySelector(".btn-scissors");
+const whoWins = document.querySelector(".who__wins");
+//
+
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
@@ -10,16 +18,12 @@ const getRandom = () => Math.round(Math.random() * (2 - 0) + 0);
 
 let toolsArr = [rock, paper, scissors];
 
-let case1 = [rock, scissors];
-let case2 = [paper, rock];
-let case3 = [scissors, paper];
+const letsPlay = (playerName = "No Name", playerChoice) => {
+  let player1 = { name: "Machine", choice: toolsArr[getRandom()] };
+  let player2 = { name: playerName, choice: playerChoice };
 
-let player1 = { name: "Machine", choice: toolsArr[getRandom()] };
-let player2 = { name: "Marco", choice: scissors };
-
-let game = [player1, player2];
-
-const letsPlay = (game) => {
+  let game = [player1, player2];
+  // console.log(game);
   let inCaseOfTie = [];
 
   let countRock = 0;
@@ -47,16 +51,26 @@ const letsPlay = (game) => {
   inCaseOfTie.sort((a, b) => b.total - a.total);
 
   if (countRock === 1 && countScissors === 1 && countPaper === 0)
-    console.log("Case 1, the winner is", whoHasRock, "with rock");
+    // console.log("Case 1, the winner is", whoHasRock, "with rock");
+    return (whoWins.textContent = `The winner is  ${whoHasRock} with rock!`);
   if (countRock === 1 && countScissors === 0 && countPaper === 1)
-    console.log("Case 2, the winner is", whoHasPaper, "with aper");
+    return (whoWins.textContent = `The winner is  ${whoHasPaper} with paper!`);
   if (countRock === 0 && countScissors === 1 && countPaper === 1)
-    console.log("Case 3, the winner is", whoHasScissors, "with Scissors");
+    return (whoWins.textContent = `The winner is  ${whoHasScissors} with scissors!`);
   // tie even
   if (countRock > 1 || countScissors > 1 || countPaper > 1)
-    console.log(
-      `TIE!!! Cause bot of them had ${inCaseOfTie.shift().tool}! Play again!`
-    );
+    return (whoWins.textContent = `TIE!!! Cause bot of them had ${
+      inCaseOfTie.shift().tool
+    }! Play again!`);
 };
 
-letsPlay(game);
+//
+btnRock.addEventListener("click", function () {
+  letsPlay(playerName.value, rock);
+});
+btnPaper.addEventListener("click", function () {
+  letsPlay(playerName.value, paper);
+});
+btnScissors.addEventListener("click", function () {
+  letsPlay(playerName.value, scissors);
+});
